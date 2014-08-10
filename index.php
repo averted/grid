80b3f6ace7b76654ce53adbb411d3c74eba1c083
averted <vitiuk.alex@gmail.com>
2014-08-10 12:21:05 -0400

    index initial commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>drag</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- Styles -->
+    <link href="/css/normalize.css" rel="stylesheet" media="screen">
+    <link href="/css/style.css" rel="stylesheet" media="screen">
+    
+    <!-- jQuery -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script src="//code.jquery.com/ui/1.11.0/jquery-ui.js"></script>
+
+</head>
+<body>
+    <div class="grid">
+
+    </div>
+    <div class="controls">
+        <ul>
+            <li class="shape" data-type="triangle">triangle</li>
+            <li class="shape" data-type="square">square</li>
+            <li class="shape" data-type="square-big">big_square</li>
+        </ul>
+    </div>
+    <script src="/js/script.js"></script>
+</body>
+</html>
