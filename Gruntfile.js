23ef41e384404a1c6fc48ea427a4cda122793dcc
averted <vitiuk.alex@gmail.com>
2015-07-10 14:04:25 -0400

    Updating build (minor)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,26 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-        concat: {
-            options: {
-                banner:"'use strict';\n"
-            },
-            dist: {
-                src: [
-                    'util/polyfill',
-                    'Grid',
-                    'Controls',
-                    'Gem',
-                    'Spark',
-                    'Shape'
-                ].map(function(file) {
-                    return 'src/'+file+'.js';
-                }),
-                dest: 'js/grid-build.js'
-            }
-        }
-    });
+  grunt.initConfig({
+    concat: {
+      options: {
+        banner:"'use strict';\n"
+      },
+      dist: {
+        src: [
+          'util/polyfill',
+          'Grid',
+          'Controls',
+          'Gem',
+          'Spark',
+          'Shape'
+        ].map(function(file) {
+          return 'src/'+file+'.js';
+        }),
+        dest: 'js/build.js'
+      }
+    }
+  });
 
-    grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
-    grunt.registerTask('default', ['concat']);
+  grunt.registerTask('default', ['concat']);
 };
