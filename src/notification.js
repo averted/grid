0bfe79e2e2b5c90140788755e4ef8a476bd3a71f
averted <vitiuk.alex@gmail.com>
2015-07-23 00:06:03 -0400

    Rebuilding notifications service

--- src/notification.js
+++ src/notification.js
@@ -1,12 +1,34 @@
+import _ from 'lodash';
+
 /**
  * Notifcation service.
  */
 var Notification = {
-  id: '.grid-notifications',
-  classList: [ 'error', 'success' ],
+  id: 'grid-notifications',
+  class: 'alert',
 
   render(options) {
-    return $(this.id).text(options.msg).removeClass(this.classList.join(' ')).addClass(options.type);
+    var el   = document.getElementById(this.id)
+      , node = document.createElement('div');
+
+    node.className += `${this.class} ${options.type}`;
+    node.innerHTML = `${options.msg}`;
+
+    el.insertBefore(node, el.firstChild);
+
+    return this.setDuration();
+  },
+
+  setDuration(time) {
+    return setTimeout(function() {
+      Notification.removeAlert();
+    }, time || 3000);
+  },
+
+  removeAlert() {
+    var lastAlert = _.last(document.getElementsByClassName(this.class));
+
+    return document.getElementById(this.id).removeChild(lastAlert);
   }
 };
 
